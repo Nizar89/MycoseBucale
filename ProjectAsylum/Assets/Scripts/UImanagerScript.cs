88aeca44893746d6c2246ef3d2c84f87b5f9c0b1
Nizar89 <azaiez.nizar@yahoo.fr>
2014-11-02 17:03:35 +0100

    Menu Julie

---
+++ ProjectAsylum/Assets/Scripts/UImanagerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UImanagerScript : MonoBehaviour 
+{
+	public void StartGame()
+	{
+		//Application.LoadLevel();
+		Application.LoadLevel(1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
