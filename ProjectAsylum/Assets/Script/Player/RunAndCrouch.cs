ba99da61264cceb5b121410e9a8ebfc073e95a23
Nizar89 <azaiez.nizar@yahoo.fr>
2014-11-02 11:54:55 +0100

    LALALA

--- ProjectAsylum/Assets/Script/Player/RunAndCrouch.cs
+++ ProjectAsylum/Assets/Script/Player/RunAndCrouch.cs
@@ -12,6 +12,8 @@ public class RunAndCrouch : MonoBehaviour
 	private float dist; // distance to ground
 	// Use this for initialization
 
+	private bool _isUsable = true;
+
 	void Awake()
 	{
 		_Player = this.gameObject;
@@ -25,27 +27,40 @@ public class RunAndCrouch : MonoBehaviour
 		CharacterController ch = GetComponent<CharacterController>();
 		dist = ch.height/2; // calculate distance to ground
 	}
+
+	void Death()
+	{
+		chMotor.movement.maxForwardSpeed = 0;
+		Destroy(this.GetComponent<MouseLook>());
+		Destroy(Camera.main.GetComponent<MouseLook>());
+		_isUsable = false;
+	}
+
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		float vScale = 1.0f;
-		float speed = walkSpeed;
-		if ((Input.GetKey("left shift") || Input.GetKey("right shift")) && chMotor.grounded)
+		if (_isUsable)
 		{
-			speed = runSpeed;
+			float vScale = 1.0f;
+			float speed = walkSpeed;
+			if ((Input.GetKey("left shift") || Input.GetKey("right shift")) && chMotor.grounded)
+			{
+				speed = runSpeed;
+			}
+			if (Input.GetKey("c"))
+			{ // press C to crouch
+				vScale = 0.5f;
+				speed = crchSpeed; // slow down when crouching
+			}
+			chMotor.movement.maxForwardSpeed = speed; // set max speed
+			float ultScale = tr.localScale.y; // crouch/stand up smoothly
+			Vector3 tmpScale = tr.localScale;
+			Vector3 tmpPosition = tr.position;
+			tmpScale.y = Mathf.Lerp(tr.localScale.y, vScale, 5 * Time.deltaTime);
+			tr.localScale = tmpScale;
+			tmpPosition.y += dist * (tr.localScale.y - ultScale); // fix vertical position
+			tr.position = tmpPosition;
 		}
-		if (Input.GetKey("c"))
-		{ // press C to crouch
-			vScale = 0.5f;
-			speed = crchSpeed; // slow down when crouching
-		}
-		chMotor.movement.maxForwardSpeed = speed; // set max speed
-		float ultScale = tr.localScale.y; // crouch/stand up smoothly
-		Vector3 tmpScale = tr.localScale;
-		Vector3 tmpPosition = tr.position;
-		tmpScale.y = Mathf.Lerp(tr.localScale.y, vScale, 5 * Time.deltaTime);
-		tr.localScale = tmpScale;
-		tmpPosition.y += dist * (tr.localScale.y - ultScale); // fix vertical position
-		tr.position = tmpPosition;
+
 	}
 }
