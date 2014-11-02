b60b7b836c34f71fa100c71b83acc6cefeaebdc8
TomGuiraud <tom_guiraud@hotmail.fr>
2014-11-02 22:40:54 +0100

    v and d menu

---
+++ ProjectAsylum/Assets/Script/WaitAndGoToMenuScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitAndGoToMenuScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("AutoToMenu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey){
+			Application.LoadLevel(0);
+		}
+	}
+
+	IEnumerator AutoToMenu (){
+		yield return new WaitForSeconds(5.0f);
+		Application.LoadLevel(0);
+	}
+}
