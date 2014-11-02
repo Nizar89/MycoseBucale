198ad2723850d5aee5224e46c22b8ce4c08e1d80
TomGuiraud <tom_guiraud@hotmail.fr>
2014-11-02 20:06:45 +0100

    ModifSound

---
+++ ProjectAsylum/Assets/Script/SoundMangerForPNJ.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundMangerForPNJ : MonoBehaviour {
+
+	public List<AudioClip> _audioClips;
+	public List<AudioClip> _voices;
+
+	private AudioSource _audioSource;
+	// Use this for initialization
+	void Start () {
+		_audioSource = this.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayAudio (int index){
+		switch(index){
+		case 1:
+			//Marche
+			_audioSource.pitch = 1;
+			_audioSource.clip = _audioClips[1];
+			_audioSource.Play();
+		break;
+		case 2:
+			//Course
+			_audioSource.pitch = 2;
+			_audioSource.clip = _audioClips[1];
+			_audioSource.Play();			
+			break;
+		case 3:
+			//Cri
+			_audioSource.pitch = 1;
+			_audioSource.clip = _audioClips[3];
+			_audioSource.Play();			
+			break;
+		case 4:
+			//Voices
+			_audioSource.pitch = 1;
+			RandomVoice();
+			break;
+		}
+	}
+
+	void RandomVoice (){
+		_audioSource.clip = _voices[Random.Range(0, _voices.Count)];
+		_audioSource.Play();
+	}
+}
