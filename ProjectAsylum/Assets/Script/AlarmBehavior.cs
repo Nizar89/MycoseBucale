e4671b5ab32e5824d0d4f375669179cd493e48d6
TomGuiraud <tom_guiraud@hotmail.fr>
2014-11-02 13:58:50 +0100

    dsqdsq

---
+++ ProjectAsylum/Assets/Script/AlarmBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmBehavior : MonoBehaviour {
+
+	public int _limitToLaunchAlarm;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ReduceAlarmLimit (){
+		_limitToLaunchAlarm --;
+		if (_limitToLaunchAlarm <=0){
+
+		}
+	}
+
+	void AlarmLaunched (){
+
+	}
+}
